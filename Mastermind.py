53db9430d35ce7e3755b85b26242402afb5014b2
LIKELENGE MOISE <fatakilikelenge@gmail.com>
2024-03-29 15:12:52 +0100

    Chiffre au hasard

--- Mastermind.py
+++ Mastermind.py
@@ -1,1 +1,19 @@
-print("hello")
+import random
+
+place1 = random.randint(0, 8)
+place2 = random.randint(0, 8)
+place3 = random.randint(0, 8)
+place4 = random.randint(0, 8)
+
+# Boucle pour éviter les doublons
+while place2 == place1:
+    place2 = random.randint(0, 8)
+
+while place3 == place2 or place3 == place1:
+    place3 = random.randint(0, 8)
+    
+while place4 == place3 or place4 == place2 or place4 == place1 :
+    place4 = random.randint(0, 8)
+
+print(place1, place2, place3, place4)
+
